3b13bf5cf9d233f9ca69a54c3a6d4bfd236537d6
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-07-17 22:10:10 -0400

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<a href="/" id="nav-home" class='sticky top-0 left-0 bg-inherit z-50 bshadow'>
+<a href="/" id="nav-home" class='sticky top-0 left-0 bg-inherit z-[200] bshadow'>
 	<div class="text-center flex  flex-col overflow-hidden items-center  justify-center">
 		<h1
 			class="relative sm:font-medium after:w-screen after:h-[1px] sm:after:h-[2px] after:right-0 p-1.5 sm:py-3 after:top-0 after:absolute after:bg-cyan-300 before:w-screen before:h-[1px] sm:before:h-[2px] before:left-0 before:bottom-0 before:absolute before:bg-cyan-300 uppercase text-3xl sm:font-extralight sm:text-6xl text-cyan-300 select-none duration-300 banana cursor-pointer my-2"
